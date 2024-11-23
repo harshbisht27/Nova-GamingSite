ec5590c66e9a979e8fc8b4b3a8b6bf5bad8d8675
harshbisht27 <harshbisht180@gmail.com>
2024-11-23 08:39:21 +0530

    base setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline orange-500'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
